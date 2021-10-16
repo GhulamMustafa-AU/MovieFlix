0afc1916c6f194c882cef8723ef33a55266ccd4c
GhulamMustafa-AU <58632244+GhulamMustafa-AU@users.noreply.github.com>
2021-10-16 19:01:45 +0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURL:"mongodb+srv://usernam:password@rest.boxvi.mongodb.net/rest?retryWrites=true&w=majority",
+    mongoURL:"mongodb+srv://username:password@rest.boxvi.mongodb.net/rest?retryWrites=true&w=majority",
 }    
